c4f607c5daeec4d47790030bc2e83c447cb7ef5c
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-27 07:28:56 -0300

    adicionado microsserviço de login

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/messages")
@@ -11,5 +9,18 @@ public class MessageResource {
     @GetMapping("/simpleMessageWelcome")
     public String simpleMessageWelcome(){
         return "BEM VINDO A AULA DE MICROSSERVIÇO USANDO SPRING BOOT !!!";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String user,
+                        @RequestParam(required = false) String password){
+        if(user.isBlank() || user.isEmpty() ||
+                password.isEmpty() || password.isBlank()){
+            return "USUÁRIO E SENHA NÃO INFORMADOS";
+        }else if(user.length() > 15 || password.length() > 15){
+            return "USUÁRIO E SENHA INVÁLIDOS";
+        }else{
+            return "LOGIN EFETUADO COM SUCESSO !!!";
+        }
     }
 }
