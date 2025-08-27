bbaf0a5c12332caa4a5716128684d781e8d212bc
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-27 06:55:31 -0300

    feat: adicionando classe de recurso de requisicao para endpoint get

--- 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
+++ 0x01/spring_boot_1/demo/src/main/java/com/example/demo/MessageResource.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
 public class MessageResource {
+
+    @GetMapping("simpleMessageWelcome")
+    public String simpleMessageWelcome(){
+        return null;
+    }
 }
