2dc6fa4f1753dc663b9ebf04a673870b974796dd
SD-W1972 <pernalongaesnobe@gmail.com>
2025-09-04 08:58:04 -0300

    feat: iniciando projeto

---
+++ 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/model/Cliente.java
@@ -0,0 +1,93 @@
+package com.example.jpa_h2_demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.List;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private int idade;
+    private String email;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Telefone> telefoneList;
+
+    @OneToMany(mappedBy = "cliente")
+    private  List<Endereco> enderecoList;
+
+    public Cliente(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+        this.telefoneList = new ArrayList<>();
+        this.enderecoList = new ArrayList<>();
+    }
+
+    public Cliente(){
+        this.telefoneList = new ArrayList<>();
+        this.enderecoList = new ArrayList<>();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public List<Endereco> getEnderecoList() {
+        return enderecoList;
+    }
+
+    public void setEnderecoList(List<Endereco> enderecoList) {
+        this.enderecoList = enderecoList;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Telefone> getTelefoneList() {
+        return telefoneList;
+    }
+
+    public void setTelefoneList(List<Telefone> telefoneList) {
+        this.telefoneList = telefoneList;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", email='" + email + '\'' +
+                ", telefoneList=" + telefoneList +
+                ", enderecoList=" + enderecoList +
+                '}';
+    }
+}
