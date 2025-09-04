6b25cce65b387353d9be9dfaa6e1b0fd227b7c51
SD-W1972 <pernalongaesnobe@gmail.com>
2025-09-04 08:21:39 -0300

    feat: adicionada entidade Telefone

---
+++ 0x04/config_JPA+H2_database/com/example/jpa_h2_demo/model/Telefone.java
@@ -0,0 +1,65 @@
+package com.example.jpa_h2_demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String DDD;
+    private int numero;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public Telefone(String DDD, int numero, Cliente cliente) {
+        this.DDD = DDD;
+        this.numero = numero;
+        this.cliente = cliente;
+    }
+
+    public Telefone(){}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDDD() {
+        return DDD;
+    }
+
+    public void setDDD(String DDD) {
+        this.DDD = DDD;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Telefone{" +
+                "id=" + id +
+                ", DDD='" + DDD + '\'' +
+                ", numero=" + numero +
+                ", cliente=" + cliente +
+                '}';
+    }
+}
