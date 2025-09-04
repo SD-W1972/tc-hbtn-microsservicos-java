4b52329319caa00ec4bb8245a0c3207eed2f58e3
SD-W1972 <pernalongaesnobe@gmail.com>
2025-09-04 09:02:01 -0300

    feat: implementado repository do telefone

---
+++ 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.example.jpa_h2_demo.repository;
+
+import com.example.jpa_h2_demo.model.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TelefoneRepository extends JpaRepository<Telefonem, UUID> {
+
+}
