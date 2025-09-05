4b1f4b6d540b164095910214b5bc1c48c83d1db3
SD-W1972 <pernalongaesnobe@gmail.com>
2025-09-05 09:03:16 -0300

    feat: metodo findById de user

--- 0x06/api-rest-userController/com/techCamps/demo/controller/UserController.java
+++ 0x06/api-rest-userController/com/techCamps/demo/controller/UserController.java
@@ -6,4 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+
+    public String findUserById(Long id){
+        if(id > 0 && id < 100){
+            return "You have entered valid ID";
+        }
+        return "You have entered invalid ID";
+    }
+
+
 }
